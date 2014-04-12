cec6c68561b535de5c420f26439582f9f5759e15
Heikki Pulkkinen <heikki.j.pulkkinen@gmail.com>
2014-04-12 19:10:44 +0300

    Koodi ei enää anna virheilmotusta

--- static/packet_to_json.js
+++ static/packet_to_json.js
@@ -2,27 +2,16 @@
 {
     var o = {};
     var a = this.serializeArray();
-    $.each(a, function() {
-	
-	var o.field = {}
-	o.field["field_id] = this.name || '';
-	
-	if (o["field"] !== undefined){
-        if (o["field"]["field_id"] !== undefined){
-			alert("Jee!");
-			if (!o["field"].push) {
-				o["field"] = [o["field"]];
-			}
-			o["field"]["field_id"].push(this.name || '');
-			o["field"]["field_value"].push(this.value || '');
+    $.each(a, function() {		
+		var oField = {}
+		oField["field_id"] = this.name || '';
+		oField["field_value"] = this.value || '';
+		
+		if (o["field"] !== undefined){
+			o["field"].push(oField);
 		} else {
-			o["field"]["field_id"] = this.name || '';
-			o["field"]["field_value"] = this.value || '';
+			o["field"] = oField;
 		}
-	} else {
-		o["field"] = this.name || '';
-	}
-	
     });
     return o;
 };
