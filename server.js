5ab9d48ecd912618bd69279a4cf59c9e37e608e3
Eero Lehtonen <eero.lehtonen@multibase.fi>
2014-04-12 17:34:09 +0200

    Korjattu post-data @ index.html

--- server.js
+++ server.js
@@ -4,19 +4,20 @@ var express   = require('express'),
     app       = express();
  
 var proxy = httpProxy.createProxyServer();
- 
-function apiProxy(host, port) {
-  return function(req, res, next) {
-    if(req.url === '/api') {
-      proxy.web(req, res, {target:'https://www.taivaanvahti.fi/api'});
-    } else {
-      next();
-    }
-  }
-}
 
-app.use(apiProxy());
+//Proxy
+app.use(function(req, res, next) {
+  if(req.url === '/api') {
+    proxy.web(req, res, {
+      target:'https://www.taivaanvahti.fi/api',
+      secure: true
+    });
+  } else {
+    next();
+  }
+});
+
+//Static stuff
 app.use(express.static(__dirname + "/static"));
 
-//static
 http.createServer(app).listen(8080);
